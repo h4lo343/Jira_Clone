28c7dbc9853877183a6296663087022962b1581b
h4lo343 <xiangnan1@student.unimelb.edu.au>
2023-01-15 14:17:51 +1100

    1/14

---
+++ front-end/src/screens/project-list/search-panel.jsx
@@ -0,0 +1,25 @@
+
+
+export const SearchPanel = ({param, setParam, users}) => {
+
+  return <form action="">
+    <div>
+      <input type="text" value={param.name} onChange={event => setParam({
+        ...param,
+        name: event.target.value
+      })}/>
+      <select value={param.personId} onChange={event => {
+        setParam({
+          ...param,
+          personId: event.target.value,
+        })
+      }
+      }>
+        <option value={""}>Manager</option>
+        {
+          users.map(user => <option key={user.id} value={user.id}>{user.name}</option>)
+        }
+      </select>
+    </div>
+  </form>
+}
